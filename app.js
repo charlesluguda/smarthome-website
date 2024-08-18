b325a48e957df54a3fea496fe03da2c6143e8ad8
Charles Luguda <charlesluguda@yahoo.com>
2024-08-18 19:31:44 +0300

    Contact Form Added

---
+++ app.js
@@ -0,0 +1,9 @@
+// ================================== FAQS SCRIPT =================================== //
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text')
+    })
+})
