5c9671d99e9901b8f449ddd16c164e4f60023ea3
Charles Luguda <charlesluguda@yahoo.com>
2024-08-18 22:12:27 +0300

    Navbar Updated

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ btns.forEach(function(btn){
         question.classList.toggle('show-text')
     })
 })
+
+//================ MENU TOGGLE SCRIPTS ===========================//
+humburger = document.querySelector(".toggle-btn");
+humburger.onclick = function(){
+  navBar = document.querySelector(".nav_links");
+  navBar.classList.toggle("showlinks");
+}
+
+window.onclick = function(event) {
+  if (!humburger.contains(event.target) && !navBar.contains(event.target)) {
+      navBar.classList.remove("showlinks");
+  }
+};
+
+const navLinks = document.querySelectorAll(".nav_links a");
+navLinks.forEach(link => {
+  link.onclick = function() {
+      navBar.classList.remove("showlinks");
+  };
+});
